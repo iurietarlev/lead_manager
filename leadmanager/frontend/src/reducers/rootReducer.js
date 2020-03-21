e4e8946abfc6b546bca07d1d2091c8a9cce40306
Iurie Tarlev <iura.tarlev@gmail.com>
2020-03-21 00:05:51 +0000

    added error handling and alerts

--- leadmanager/frontend/src/reducers/rootReducer.js
+++ leadmanager/frontend/src/reducers/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import leadsReducer from './leadsReducer';
+import errorsReducer from './errorsReducer';
+import messagesReducer from './messagesReducer';
 
 export default combineReducers({
-  leads: leadsReducer
+  leads: leadsReducer,
+  errors: errorsReducer,
+  messages: messagesReducer
 });
